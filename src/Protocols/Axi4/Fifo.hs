6a96899e0b21522aa50a4b75c1827a4c2f120567
Sam Alws <sam@samalws.com>
2022-06-02 10:53:50 -0400

    axi4 fifo source

--- src/Protocols/Axi4/Fifo.hs
+++ src/Protocols/Axi4/Fifo.hs
@@ -41,8 +41,73 @@ import qualified Clash.Explicit.Prelude     as E
 -- TODO change all above
 
 
--- | Wishbone to Df sink
---
+-- | Axi4 to Df source
+-- TODO change comment
+-- * Reading from the given address, pops an item from the fifo
+-- * Writes are acknowledged, but ignored
+-- * Reading from statusAddress returns how much free space is left in the buffer
+-- * Reading/writing to any other address is acknowledged, but the fifo element is not popped.
+-- * Asserts err when the FIFO is empty
+axi4Source ::
+  (1 + n) ~ depth =>
+  HiddenClockResetEnable dom =>
+  KnownNat depth =>
+  KnownNat (BitSize dat) =>
+  KnownNat (Width aw) =>
+  KnownNat (Width iw) =>
+  NFDataX dat =>
+  BitPack dat =>
+  -- | Address to respond to
+  BitVector (Width aw) ->
+  -- | Depth of the FIFO
+  SNat depth ->
+  -- |
+  Circuit
+    (Axi4WriteAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos (),
+     Axi4WriteData dom 'NoStrobe ((BitSize dat + 7) `Div` 8) ())
+    (Df dom dat)
+axi4Source respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
+
+  circuitFunction reset ((inpA, inpB), inpC) = ((otpA, otpB), otpC) where
+    (otpA, otpB, otpC) = unbundle $ mealy machineAsFunction s0 $ bundle (unsafeToHighPolarity reset, inpA, inpB, inpC)
+
+  machineAsFunction s i = (s',o) where (o,s') = runState (fullStateMachine i) s
+
+  s0 = (False, NoData, replicate fifoDepth NoData)
+
+  fullStateMachine (True,_,_,_) = pure (S2M_WriteAddress{_awready = False}, S2M_WriteData{_wready = False}, NoData)
+  fullStateMachine (False,addrM2S,dataM2S,Ack ack) = do
+    ackA <- processAddress addrM2S
+    ackB <- pushInpData dataM2S
+    sendData
+    (_, dataOut, _) <- get
+    clearData ack
+    pure (ackA, ackB, dataOut)
+
+  processAddress M2S_NoWriteAddress = pure (S2M_WriteAddress{_awready = False})
+  processAddress addrM2S = do
+    (_,b,c) <- get
+    put (_awaddr addrM2S == respondAddress, b, c)
+    pure (S2M_WriteAddress{_awready = True})
+
+  pushInpData M2S_NoWriteData = pure (S2M_WriteData{_wready = False})
+  pushInpData inpDat = do
+    (shouldRead,b,buf) <- get
+    when shouldRead $ put (False,b,Data (unpack $ resize $ _wdata inpDat) +>> buf)
+    pure (S2M_WriteData{_wready = True})
+
+  sendData = do
+    (a,currOtp,buf) <- get
+    case currOtp of
+      NoData -> put (a, E.head buf, buf <<+ NoData)
+      _ -> pure ()
+
+  clearData ack = when ack $ do
+    (a,_,c) <- get
+    put (a, NoData, c)
+
+-- | Axi4 to Df sink
+-- TODO change comment
 -- * Reading from the given address, pops an item from the fifo
 -- * Writes are acknowledged, but ignored
 -- * Reading from statusAddress returns how much free space is left in the buffer
@@ -63,7 +128,7 @@ axi4Sink ::
   -- |
   Circuit
     (Df dom dat)
-    (Reverse (Axi4ReadAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos dat),
+    (Reverse (Axi4ReadAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos ()),
      Axi4ReadData dom 'NoResponse iw () dat)
 axi4Sink respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
 
@@ -74,12 +139,13 @@ axi4Sink respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
 
   s0 = (False, S2M_NoReadData, replicate fifoDepth NoData)
 
-  fullStateMachine (True,_,_,_) = pure (Ack False, S2M_ReadAddress{_arready = True}, S2M_NoReadData{})
+  fullStateMachine (True,_,_,_) = pure (Ack False, S2M_ReadAddress{_arready = False}, S2M_NoReadData{})
   fullStateMachine (False,inpDat,addrM2S,dataM2S) = do
     ackA <- pushInpData inpDat
-    ackB <- processReadAddress addrM2S
-    sendDataS2M dataM2S
+    ackB <- processAddress addrM2S
+    sendData
     (_, dataS2M, _) <- get
+    clearData dataM2S
     pure (ackA, ackB, dataS2M)
 
   pushInpData NoData = pure (Ack False)
@@ -88,17 +154,18 @@ axi4Sink respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
     put (a,b,inpDat +>> buf)
     pure (Ack True)
 
-  processReadAddress M2S_NoReadAddress = pure (S2M_ReadAddress{_arready = False})
-  processReadAddress addrM2S = do
+  processAddress M2S_NoReadAddress = pure (S2M_ReadAddress{_arready = False})
+  processAddress addrM2S = do
     (_,b,c) <- get
     put (_araddr addrM2S == respondAddress, b, c)
     pure (S2M_ReadAddress{_arready = True})
 
-  sendDataS2M dataM2S | _rready dataM2S = do
-    (_,_,c) <- get
-    put (False, S2M_NoReadData, c)
-  sendDataS2M _ = do
+  sendData = do
     (shouldSend,currOtp,buf) <- get
     case (shouldSend, currOtp, E.head buf) of
       (True, S2M_NoReadData, Data toSend) -> put (False, S2M_ReadData { _rid = 0, _rdata = toSend, _rresp = (), _rlast = True, _ruser = () }, buf <<+ NoData)
       _ -> pure ()
+
+  clearData dataM2S = when (_rready dataM2S) $ do
+    (_,_,c) <- get
+    put (False, S2M_NoReadData, c)
