f9d693a3657d11d7ce5e3b40a5e44e8e1fc7ba08
Sam Alws <sam@samalws.com>
2022-06-02 16:23:00 -0400

    added status addresses on axi4 fifos

--- src/Protocols/Axi4/Fifo.hs
+++ src/Protocols/Axi4/Fifo.hs
@@ -19,7 +19,7 @@ import GHC.Generics (Generic)
 
 -- clash-prelude
 -- import qualified Clash.Prelude as C
-import Clash.Prelude hiding (pure, not)
+import Clash.Prelude hiding (pure, not, (&&))
 
 -- me
 import Protocols.Axi4.Common hiding (Data)
@@ -50,6 +50,7 @@ import qualified Clash.Explicit.Prelude     as E
 -- * Asserts err when the FIFO is empty
 axi4Source ::
   (1 + n) ~ depth =>
+  (depth+1) ~ (1+depth) =>
   HiddenClockResetEnable dom =>
   KnownNat depth =>
   KnownNat (BitSize dat) =>
@@ -59,52 +60,70 @@ axi4Source ::
   BitPack dat =>
   -- | Address to respond to
   BitVector (Width aw) ->
+  -- | TODO
+  Maybe (BitVector (Width aw)) ->
   -- | Depth of the FIFO
   SNat depth ->
   -- |
   Circuit
     (Axi4WriteAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos (),
-     Axi4WriteData dom 'NoStrobe ((BitSize dat + 7) `Div` 8) ())
+     Axi4WriteData dom 'NoStrobe ((BitSize dat + 7) `Div` 8) (),
+     Axi4ReadAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos (),
+     Reverse (Axi4ReadData dom 'NoResponse iw () (Index (depth+1))))
     (Df dom dat)
-axi4Source respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
+axi4Source respondAddress statusAddress fifoDepth = Circuit (hideReset circuitFunction) where
 
-  circuitFunction reset ((inpA, inpB), inpC) = ((otpA, otpB), otpC) where
-    (otpA, otpB, otpC) = unbundle $ mealy machineAsFunction s0 $ bundle (unsafeToHighPolarity reset, inpA, inpB, inpC)
+  circuitFunction reset ((inpA, inpB, inpC, inpD), inpE) = ((otpA, otpB, otpC, otpD), otpE) where
+    (otpA, otpB, otpC, otpD, otpE) = unbundle $ mealy machineAsFunction s0 $ bundle (unsafeToHighPolarity reset, inpA, inpB, inpC, inpD, inpE)
 
   machineAsFunction s i = (s',o) where (o,s') = runState (fullStateMachine i) s
 
-  s0 = (NoData, replicate fifoDepth NoData)
+  s0 = (False, S2M_NoReadData, NoData, maxBound, replicate fifoDepth NoData)
 
-  fullStateMachine (True,_,_,_) = pure (S2M_WriteAddress{_awready = False}, S2M_WriteData{_wready = False}, NoData)
-  fullStateMachine (False,addrM2S,dataM2S,Ack ack) = do
+  fullStateMachine (True,_,_,_,_,_) = pure (S2M_WriteAddress{_awready = False}, S2M_WriteData{_wready = False}, S2M_ReadAddress{_arready = False}, S2M_NoReadData, NoData)
+  fullStateMachine (False,addrM2S,dataM2S,statusAddrM2S,statusAckM2S,Ack ack) = do
     ackA <- processAddress addrM2S
     ackB <- pushInpData dataM2S
+    ackC <- sendStatus statusAddrM2S
     sendData
-    (dataOut, _) <- get
+    (_, statusOut, dataOut, _, _) <- get
+    clearStatus statusAckM2S
     clearData ack
-    pure (ackA, ackB, dataOut)
+    pure (ackA, ackB, ackC, statusOut, dataOut)
 
   processAddress M2S_NoWriteAddress = pure (S2M_WriteAddress{_awready = False})
   processAddress addrM2S = do
-    (_,b,c) <- get
-    put (_awaddr addrM2S == respondAddress, b, c)
+    (_,b,c,d,e) <- get
+    put (_awaddr addrM2S == respondAddress, b, c, d, e)
     pure (S2M_WriteAddress{_awready = True})
 
   pushInpData M2S_NoWriteData = pure (S2M_WriteData{_wready = False})
   pushInpData inpDat = do
-    (shouldRead,b,buf) <- get
-    when shouldRead $ put (False,b,Data (unpack $ resize $ _wdata inpDat) +>> buf)
-    pure (S2M_WriteData{_wready = True})
+    (shouldRead,b,c,numFree,buf) <- get
+    when (shouldRead && numFree > 0) $ put (False,b,c,numFree-1,Data (unpack $ resize $ _wdata inpDat) +>> buf)
+    pure (S2M_WriteData{_wready = numFree > 0})
+
+  sendStatus M2S_NoReadAddress = pure (S2M_ReadAddress { _arready = False })
+  sendStatus ack = do
+    (a,statusOut,c,numFree,e) <- get
+    case (Just (_araddr ack) == statusAddress, statusOut) of
+      (True, S2M_NoReadData) -> put (a,S2M_ReadData { _rdata = numFree, _rid = 0, _rresp = (), _rlast = True, _ruser = () },c,numFree,e)
+      _ -> pure ()
+    pure (S2M_ReadAddress { _arready = True })
 
   sendData = do
-    (a,currOtp,buf) <- get
-    case currOtp of
-      NoData -> put (a, E.head buf, buf <<+ NoData)
+    (a,b,currOtp,numFree,buf) <- get
+    case (currOtp,numFree == maxBound) of
+      (NoData, False) -> put (a, b, E.head buf, numFree+1, buf <<+ NoData)
       _ -> pure ()
 
+  clearStatus ack = when (_rready ack) $ do
+    (a,_,c,d,e) <- get
+    put (a, S2M_NoReadData, c, d, e)
+
   clearData ack = when ack $ do
-    (a,_,c) <- get
-    put (a, NoData, c)
+    (a,b,_,d,e) <- get
+    put (a, b, NoData, d, e)
 
 -- | Axi4 to Df sink
 -- TODO change comment
@@ -123,44 +142,50 @@ axi4Sink ::
   NFDataX dat =>
   -- | Address to respond to
   BitVector (Width aw) ->
+  -- | TODO
+  Maybe (BitVector (Width aw)) ->
   -- | Depth of the FIFO
   SNat depth ->
   -- |
   Circuit
     (Df dom dat)
     (Reverse (Axi4ReadAddress dom 'NoBurst 'NoSize lw iw aw 'NoRegion 'NoBurstLength 'NoLock 'NoCache 'NoPermissions 'NoQos ()),
-     Axi4ReadData dom 'NoResponse iw () dat)
-axi4Sink respondAddress fifoDepth = Circuit (hideReset circuitFunction) where
+     Axi4ReadData dom 'NoResponse iw () (Either (Index (depth+1)) dat))
+axi4Sink respondAddress statusAddress fifoDepth = Circuit (hideReset circuitFunction) where
 
   circuitFunction reset (inpA, (inpB, inpC)) = (otpA, (otpB, otpC)) where
     (otpA, otpB, otpC) = unbundle $ mealy machineAsFunction s0 $ bundle (unsafeToHighPolarity reset, inpA, inpB, inpC)
 
   machineAsFunction s i = (s',o) where (o,s') = runState (fullStateMachine i) s
 
-  s0 = (S2M_NoReadData, replicate fifoDepth NoData)
+  s0 = (S2M_NoReadData, maxBound, replicate fifoDepth NoData)
 
   fullStateMachine (True,_,_,_) = pure (Ack False, S2M_ReadAddress{_arready = False}, S2M_NoReadData{})
   fullStateMachine (False,inpDat,addrM2S,dataM2S) = do
     ackA <- pushInpData inpDat
     ackB <- sendData addrM2S
-    (dataS2M, _) <- get
+    (dataS2M, _, _) <- get
     clearData dataM2S
     pure (ackA, ackB, dataS2M)
 
   pushInpData NoData = pure (Ack False)
   pushInpData inpDat@(Data _) = do
-    (a,buf) <- get
-    put (a,inpDat +>> buf)
-    pure (Ack True)
+    (a,numFree,buf) <- get
+    if numFree == 0 then pure (Ack False) else do
+      put (a,numFree-1,inpDat +>> buf)
+      pure (Ack True)
 
   sendData M2S_NoReadAddress = pure (S2M_ReadAddress{_arready = False})
   sendData addrM2S = do
-    (currOtp,buf) <- get
-    case (_araddr addrM2S == respondAddress, currOtp, E.head buf) of
-      (True, S2M_NoReadData, Data toSend) -> put (S2M_ReadData { _rid = 0, _rdata = toSend, _rresp = (), _rlast = True, _ruser = () }, buf <<+ NoData)
+    (currOtp,numFree,buf) <- get
+    case (_araddr addrM2S == respondAddress, Just (_araddr addrM2S) == statusAddress, currOtp, E.head buf) of
+      (True, _, S2M_NoReadData, Data toSend) ->
+        put (S2M_ReadData { _rid = 0, _rdata = Right toSend, _rresp = (), _rlast = True, _ruser = () }, numFree+1, buf <<+ NoData)
+      (_, True, S2M_NoReadData, _) ->
+        put (S2M_ReadData { _rid = 0, _rdata = Left numFree, _rresp = (), _rlast = True, _ruser = () }, numFree, buf)
       _ -> pure ()
     pure (S2M_ReadAddress{_arready = True})
 
   clearData dataM2S = when (_rready dataM2S) $ do
-    (_,b) <- get
-    put (S2M_NoReadData, b)
+    (_,b,c) <- get
+    put (S2M_NoReadData,b,c)
