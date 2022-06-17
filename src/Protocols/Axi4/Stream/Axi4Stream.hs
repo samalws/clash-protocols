e14161a404804a3603cb0726a2f57b82ab4c834d
Sam Alws <sam@samalws.com>
2022-06-17 13:33:14 -0400

    axi4stream mostly done aside from comments, still not sure if the way I did dfLike is good

--- src/Protocols/Axi4/Stream/Axi4Stream.hs
+++ src/Protocols/Axi4/Stream/Axi4Stream.hs
@@ -19,18 +19,17 @@ import           Protocols.Internal
 import           Protocols.DfLike (DfLike)
 import qualified Protocols.DfLike as DfLike
 
-data Axi4StreamByte = DataByte (Unsigned 8) | PositionByte {- TODO no data? -} | NullByte  deriving (Generic, C.NFDataX, C.ShowX, Eq, NFData, Show)
+data Axi4StreamByte = DataByte (Unsigned 8) | PositionByte | NullByte  deriving (Generic, C.NFDataX, C.ShowX, Eq, NFData, Show)
 
--- TODO dataType should *only* ever be Vec n Axi4StreamByte; how do I enforce this?
 data Axi4StreamM2S idWidth destWidth userType dataType
-  = Axi4StreamM2S
+  = NoAxi4StreamM2S
+  | Axi4StreamM2S
   {
-    streamBytes :: dataType, -- TODO what to name it?
-    tValid      :: Bool,
-    tLast       :: Bool,
-    tId         :: Unsigned idWidth,
-    tDest       :: Unsigned destWidth,
-    tUser       :: userType
+    tData :: dataType,
+    tLast :: Bool,
+    tId   :: Unsigned idWidth,
+    tDest :: Unsigned destWidth,
+    tUser :: userType
   }
   deriving (Generic, C.NFDataX, C.ShowX, Eq, NFData, Show)
 
@@ -38,14 +37,14 @@ data Axi4StreamS2M = Axi4StreamS2M { tReady :: Bool } deriving (Generic, C.NFDat
 
 data Axi4Stream (dom :: Domain) (idWidth :: Nat) (destWidth :: Nat) (userType :: Type) (dataType :: Type)
 
-instance Protocol (Axi4Stream dom idWidth destWidth userType dataType) where
+instance (dataType ~ Vec dataLen Axi4StreamByte) => Protocol (Axi4Stream dom idWidth destWidth userType dataType) where
   type Fwd (Axi4Stream dom idWidth destWidth userType dataType) = Signal dom (Axi4StreamM2S idWidth destWidth userType dataType)
   type Bwd (Axi4Stream dom idWidth destWidth userType dataType) = Signal dom Axi4StreamS2M
 
-instance Backpressure (Axi4Stream dom idWidth destWidth userType dataType) where
+instance (dataType ~ Vec dataLen Axi4StreamByte) => Backpressure (Axi4Stream dom idWidth destWidth userType dataType) where
   boolsToBwd _ = C.fromList_lazy . fmap Axi4StreamS2M
 
-instance (C.KnownDomain dom{-, C.NFDataX a, C.ShowX a, Show a-}) => Simulate (Axi4Stream dom idWidth destWidth userType dataType) where
+instance (dataType ~ Vec dataLen Axi4StreamByte, C.KnownDomain dom, KnownNat idWidth, KnownNat destWidth) => Simulate (Axi4Stream dom idWidth destWidth userType dataType) where
   type SimulateFwdType (Axi4Stream dom idWidth destWidth userType dataType) = [Axi4StreamM2S idWidth destWidth userType dataType]
   type SimulateBwdType (Axi4Stream dom idWidth destWidth userType dataType) = [Axi4StreamS2M]
   type SimulateChannels (Axi4Stream dom idWidth destWidth userType dataType) = 1
@@ -55,35 +54,33 @@ instance (C.KnownDomain dom{-, C.NFDataX a, C.ShowX a, Show a-}) => Simulate (Ax
   sigToSimFwd _ = C.sample_lazy
   sigToSimBwd _ = C.sample_lazy
 
-  stallC conf (C.head -> (stallAck, stalls)) = DfLike.stall Proxy conf stallAck stalls -- TODO
+  stallC conf (C.head -> (stallAck, stalls)) = DfLike.stall Proxy conf stallAck stalls
 
 streamM2SToMaybe :: Axi4StreamM2S idWidth destWidth userType dataType -> Maybe dataType
-streamM2SToMaybe m2s = if tValid m2s then Just (streamBytes m2s) else Nothing
+streamM2SToMaybe NoAxi4StreamM2S = Nothing
+streamM2SToMaybe m2s = Just (tData m2s)
 
-instance (C.KnownDomain dom, KnownNat idWidth, KnownNat destWidth) => Drivable (Axi4Stream dom idWidth destWidth userType dataType) where
+instance (dataType ~ Vec dataLen Axi4StreamByte, C.KnownDomain dom, KnownNat idWidth, KnownNat destWidth) => Drivable (Axi4Stream dom idWidth destWidth userType dataType) where
   type ExpectType (Axi4Stream dom idWidth destWidth userType dataType) = [dataType]
 
-  toSimulateType Proxy = f 0 where
-    f i (a:b)
-      = (Axi4StreamM2S { streamBytes = a, tValid = True, tLast = True, tId = i, tDest = 0 {- TODO -}, tUser = P.undefined {- TODO -} })
-      : (f (i+1) b) -- TODO do we really want to be incrementing id every time?
-    f _ [] = P.error {- TODO errorX? -} "Finite list"
+  toSimulateType Proxy = fmap (\dat -> (Axi4StreamM2S { tData = dat, tLast = False, tId = 0, tDest = 0, tUser = P.undefined }))
   fromSimulateType Proxy = Maybe.mapMaybe streamM2SToMaybe
 
-  driveC = DfLike.drive Proxy -- TODO
-  sampleC = DfLike.sample Proxy -- TODO
+  driveC = DfLike.drive Proxy
+  sampleC = DfLike.sample Proxy
 
-instance DfLike dom (Axi4Stream dom idWidth destWidth userType) dataType where
+instance (dataType ~ Vec dataLen Axi4StreamByte, KnownNat idWidth, KnownNat destWidth) => DfLike dom (Axi4Stream dom idWidth destWidth userType) dataType where
   type Data (Axi4Stream dom idWidth destWidth userType) dataType = Axi4StreamM2S idWidth destWidth userType dataType
   type Payload dataType = dataType
   type Ack (Axi4Stream dom idWidth destWidth userType) dataType = Axi4StreamS2M
 
   getPayload = const $ streamM2SToMaybe
 
-  setPayload _ _ m2s (Just b) = m2s { tValid = True, streamBytes = b }
-  setPayload _ _ m2s Nothing = m2s { tValid = False, streamBytes = Prelude.undefined }
+  setPayload _ _ NoAxi4StreamM2S (Just b) = Axi4StreamM2S { tData = b, tLast = False, tId = 0, tDest = 0, tUser = P.undefined }
+  setPayload _ _ m2s (Just b) = m2s { tData = b }
+  setPayload _ _ _ Nothing = NoAxi4StreamM2S
 
-  noData _ = Axi4StreamM2S { tValid = False }
+  noData _ = NoAxi4StreamM2S
 
   boolToAck _ = Axi4StreamS2M
   ackToBool _ = tReady
