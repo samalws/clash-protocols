8a95c3459f29e4ebb32af43b2a24be79e36532b6
Sam Alws <sam@samalws.com>
2022-07-13 17:07:34 -0400

    start to convert Test stuff

--- src/Protocols/Df.hs
+++ src/Protocols/Df.hs
@@ -78,8 +78,9 @@ import qualified Prelude as P
 
 -- clash-prelude
 import           Clash.Prelude (type (<=))
-import           Clash.Signal.Internal (Signal)
+import           Clash.Signal.Internal (Signal(..))
 import qualified Clash.Prelude as C
+import qualified Clash.Explicit.Prelude as CE
 
 -- me
 import           Protocols.Internal
@@ -718,6 +719,7 @@ fifo fifoDepth = Circuit $ C.hideReset circuitFunction where
 
 --------------------------------- SIMULATE -------------------------------------
 
+{-
 -- | Emit values given in list. Emits no data while reset is asserted. Not
 -- synthesizable.
 drive ::
@@ -776,3 +778,139 @@ simulate ::
   -- | Outputs
   [Data b]
 simulate = DfLike.simulate Proxy Proxy
+-}
+
+-- | Emit values given in list. Emits no data while reset is asserted. Not
+-- synthesizable.
+drive ::
+  forall dom df a.
+  ( C.KnownDomain dom ) =>
+  SimulationConfig ->
+  [Data a] -> -- TODO
+  Circuit () (Df dom a)
+drive dfA SimulationConfig{resetCycles} s0 = Circuit $
+    ((),)
+  . C.fromList_lazy
+  . go s0 resetCycles
+  . CE.sample_lazy
+  . P.snd
+ where
+  go _ resetN  ~(ack:acks) | resetN > 0 =
+    NoData dfA : (ack `C.seqX` go s0 (resetN - 1) acks)
+  go [] _ ~(ack:acks) =
+    NoData : (ack `C.seqX` go [] 0 acks)
+  go (NoData:is)  _ ~(ack:acks) =
+    NoData : (ack `C.seqX` go is 0 acks)
+  go (Data dat:is)  _ ~(Ack ack:acks) =
+    Data dat : go (if ack then is else dat:is) 0 acks
+
+-- | Sample protocol to a list of values. Drops values while reset is asserted.
+-- Not synthesizable.
+--
+-- For a generalized version of 'sample', check out 'sampleC'.
+sample ::
+  forall dom df b.
+  ( C.KnownDomain dom ) =>
+  SimulationConfig ->
+  Circuit () (Df dom b) ->
+  [Data b] -- TODO should be maybe
+sample dfB SimulationConfig{..} c =
+    P.take timeoutAfter
+  $ CE.sample_lazy
+  $ ignoreWhileInReset
+  $ P.snd
+  $ toSignals c ((), Ack <$> rst_n)
+ where
+  ignoreWhileInReset s =
+    (uncurry (bool NoData)) <$>
+    C.bundle (s, rst_n)
+
+  rst_n = C.fromList (replicate resetCycles False <> repeat True)
+
+-- | Stall every valid Df packet with a given number of cycles. If there are
+-- more valid packets than given numbers, passthrough all valid packets without
+-- stalling. Not synthesizable.
+--
+-- For a generalized version of 'stall', check out 'stallC'.
+stall ::
+  forall dom df a.
+  ( C.KnownDomain dom
+  , HasCallStack ) =>
+  SimulationConfig ->
+  -- | Acknowledgement to send when LHS does not send data. Stall will act
+  -- transparently when reset is asserted.
+  StallAck ->
+  -- Number of cycles to stall for every valid Df packet
+  [Int] ->
+  Circuit (Df dom a) (Df dom a)
+stall dfA SimulationConfig{..} stallAck stalls = Circuit $
+  uncurry (go stallAcks stalls resetCycles)
+ where
+  stallAcks
+    | stallAck == StallCycle = [minBound..maxBound] \\ [StallCycle]
+    | otherwise = [stallAck]
+
+  toStallAck :: Maybe a -> Ack -> StallAck -> Ack
+  toStallAck (Just _) ack = P.const ack
+  toStallAck Nothing ack = \case
+    StallWithNack -> Ack False
+    StallWithAck -> Ack True
+    StallWithErrorX -> C.errorX "No defined ack"
+    StallTransparently -> ack
+    StallCycle -> Ack False -- shouldn't happen..
+
+  go ::
+    [StallAck] ->
+    [Int] ->
+    Int ->
+    Signal dom (Data a) ->
+    Signal dom Ack ->
+    ( Signal dom Ack
+    , Signal dom (Data a) )
+  go [] ss rs fwd bwd =
+    go stallAcks ss rs fwd bwd
+
+  go (_:sas) _ resetN (f :- fwd) ~(b :- bwd) | resetN > 0 =
+    B.bimap (b :-) (f :-) (go sas stalls (resetN - 1) fwd bwd)
+
+  go (sa:sas) [] _ (f :- fwd) ~(b :- bwd) =
+    B.bimap (toStallAck (dataToMaybe f) b sa :-) (f :-) (go sas [] 0 fwd bwd)
+
+  go (sa:sas) ss _ (NoData :- fwd) ~(b :- bwd) =
+    -- Left hand side does not send data, simply replicate that behavior. Right
+    -- hand side might send an arbitrary acknowledgement, so we simply pass it
+    -- through.
+    B.bimap (toStallAck Nothing b sa :-) (NoData :-) (go sas ss 0 fwd bwd)
+  go (_sa:sas) (s:ss) _ (f0 :- fwd) ~(Ack b0 :- bwd) =
+    let
+      -- Stall as long as s > 0. If s ~ 0, we wait for the RHS to acknowledge
+      -- the data. As long as RHS does not acknowledge the data, we keep sending
+      -- the same data.
+      (f1, b1, s1) = case compare 0 s of
+        LT -> (NoData, Ack False, pred s:ss)    -- s > 0
+        EQ -> (f0, Ack b0, if b0 then ss else s:ss) -- s ~ 0
+        GT -> error ("Unexpected negative stall: " <> show s) -- s < 0
+    in
+      B.bimap (b1 :-) (f1 :-) (go sas s1 0 fwd bwd)
+
+-- | Simulate a single domain protocol. Not synthesizable.
+--
+-- For a generalized version of 'simulate', check out 'Protocols.simulateC'.
+simulate ::
+  forall dom df a b.
+  ( C.KnownDomain dom ) =>
+  -- | Simulation configuration. Use 'Data.Default.def' for sensible defaults.
+  SimulationConfig ->
+  -- | Circuit to simulate.
+  ( C.Clock dom ->
+    C.Reset dom ->
+    C.Enable dom ->
+    Circuit (Df dom a) (Df dom b) ) ->
+  -- | Inputs
+  [Data a] ->
+  -- | Outputs
+  [Data b]
+simulate dfA dfB conf@SimulationConfig{..} circ inputs =
+  sample dfB conf (drive dfA conf inputs |> circ clk rst ena)
+ where
+  (clk, rst, ena) = (C.clockGen, resetGen resetCycles, C.enableGen)
